8138e5076ebe4f13fd3a8bdaa435d1bdf78db89b
Xin Liu <liu0025@hz.nl>
2017-03-21 09:55:13 +0100

    Python_learn: XML

--- flasktest/main.py
+++ flasktest/main.py
@@ -1,16 +1,31 @@
 from multiprocessing import Process
 import os
 
-def run_proc(name):
-    print('Run child process %s (%s)...' % (name, os.getpid()))
+from xml.parsers.expat import ParserCreate
 
-if __name__=='__main__':
-    print('Parent process %s.' % os.getpid())
-    p = Process(target=run_proc, args=('test',))
-    print('Child process will start.')
-    p.start()
-    p.join()
-    print('Child process  end')
+class DefaultSaxHandler(object):
+    def start_element(self, name, attrs):
+        print('sax:start_element: %s, attrs: %s' % (name, str(attrs)))
+
+    def end_element(self, name):
+        print('sax:end_element: %s' % name)
+
+    def char_data(self, text):
+        print('sax:char_data: %s' % text)
+
+xml = r'''<?xml version="1.0"?>
+<ol>
+    <li><a href="/python">Python</a></li>
+    <li><a href="/ruby">Ruby</a></li>
+</ol>
+'''
+
+handler = DefaultSaxHandler()
+parser = ParserCreate()
+parser.StartElementHandler = handler.start_element
+parser.EndElementHandler = handler.end_element
+parser.CharacterDataHandler = handler.char_data
+parser.Parse(xml)
 
 #if __name__ == "__main__":
 #    app.run(debug=True , host='0.0.0.0')
