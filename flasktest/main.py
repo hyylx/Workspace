853309d5e7543780cd234d240a0dda4986eb3ae8
Xin Liu <liu0025@hz.nl>
2017-03-24 14:10:17 +0100

    Python_learn: WSGI接口

--- flasktest/main.py
+++ flasktest/main.py
@@ -1,20 +1,7 @@
-from email.mime.text import MIMEText
-
-msg = MIMEText('hello!','plain', 'utf-8')
-# 构造MIMEText对象时，第一个参数是邮件正文，第二个参数是MIME的subtype('plain'表示纯文本，utf-8保证多语言兼容性)
-
-from_addr = input('from: ')
-password = input('Password: ')
-to_addr = input('To: ')
-
-smtp_server = input('SMTP server: ')
-
-import smtplib
-server = smtplib.SMTP(smtp_server,25)#默认端口25
-server.set_debuglevel(1)# 打印出和SMTP服务器交互的所有信息
-server.login(from_addr, password)
-server.sendmail(from_addr,[to_addr],msg.as_string())#收件人是一个list因为可以一次发给多个人| 邮件正文要是str
-server.quit()
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    body = '<h1>Hello, %s!</h1>' % (environ['PATH_INFO'][1:] or 'web')
+    return [body.encode('utf-8')]
 
 #if __name__ == "__main__":
 #    app.run(debug=True , host='0.0.0.0')
