35e200a0c05d81cb87dea8884888a9447b27fad4
Xin Liu <liu0025@hz.nl>
2017-03-24 14:46:38 +0100

    Python_learn: flask local test ---- home+signin

--- flasktest/main.py
+++ flasktest/main.py
@@ -1,8 +1,27 @@
-def application(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    body = '<h1>Hello, %s!</h1>' % (environ['PATH_INFO'][1:] or 'web')
-    return [body.encode('utf-8')]
+from flask import Flask
+from flask import request
 
-#if __name__ == "__main__":
-#    app.run(debug=True , host='0.0.0.0')
+app = Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    return '<h1>Home</h1>'
+
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return '''<form action="/signin"method="post">
+             <p><input name="username"></p>
+             <p><input name="password" type="password"></p>
+             <p><button type="submit">Sign In</button></p>
+             </form>'''
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    if request.form['username']=='xin' and request.form['password']=='password':
+        return '<h3>Hello!</h3>'
+    else:
+        return '<h3>No!</h3>'
+
+if __name__ == "__main__":
+    app.run()#debug=True , host='0.0.0.0')
 
