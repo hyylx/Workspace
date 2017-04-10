dcbe28d3ec665098f46033261f56e3198d33d48f
Xin Liu <liu0025@hz.nl>
2017-04-10 15:00:20 +0200

    removed old location file made a nice quppa driver start

--- flasktest/gw_quppa.py
+++ flasktest/gw_quppa.py
@@ -1,11 +1,30 @@
+"""
+VTEC Quppa driver
+Get interface to Quppa
+author: Xin
+date:
+License:
+"""
+
 from urllib import request
 import time
 import json
 
 
-class VTECQuppaAPI(object):
+class VTECQuppa(object):
+    """
+    your comment here
+    """
     def __init__(self, ip='192.168.0.142'):
         self.ip = ip
+        self.tag_id = None
+
+    def set_tag_id(self, id):
+        """
+        Set your tag id here
+        id: int tag number
+        """
+        self.tag_id = id
 
     def get_location(self):
         with request.urlopen(
@@ -24,9 +43,27 @@ class VTECQuppaAPI(object):
     def read_quppa(self):
         pass
 
+    def get_battery_voltage(self):
+        pass
+
+    def get_status(self):
+        if self.get_battery_voltage():
+            pass
+
+        if self.get_location():
+            pass
+
+        return "OPERATIONAL"
+
+    def get_diameter(self):
+        self.get_location()
+        #TODO calculate a diemsion
 
 if __name__ == "__main__":
-    new_quppa = VTECQuppaAPI()
+    new_quppa = VTECQuppa()
+    new_quppa.set_tag_id(2)
     new_quppa.get_location()
     time.sleep(3)
     new_quppa.get_location()
+    new_quppa.get_diameter()
+    new_quppa.get_battery_voltage()
