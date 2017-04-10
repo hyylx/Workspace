5868ca26908bdde7bda69e5a269362fd6b856716
Xin Liu <liu0025@hz.nl>
2017-04-10 11:53:52 +0200

    get tag position with JSON formet from QPE via HTTP request

--- flasktest/get_location_QPE.py
+++ flasktest/get_location_QPE.py
@@ -1,6 +1,10 @@
 from urllib import request
+import time
 
-with request.urlopen('http://192.168.0.142:8080/qpe/getTagPosition?version=2&humanReadable=true&maxAge=5000') as f:
-    data = f.read()
-    print('Data:', data.decode('utf-8'))
-
+time_count = 0
+while time_count < 5:
+    with request.urlopen('http://192.168.0.142:8080/qpe/getTagPosition?version=2&humanReadable=true&maxAge=2000') as f:
+        data = f.read()
+        print('Data:', data.decode('utf-8'))
+    time_count += 1
+    time.sleep(2)
