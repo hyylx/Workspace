684d625fcda52217c50ad70c4bc03b2e2521c6f9
Xin Liu <liu0025@hz.nl>
2017-04-11 16:57:15 +0200

    make 'read_quuppa_tagposition' and 'read_quuppa_taginfo' into 2 method  create get_tag_message

--- flasktest/gw_quppa.py
+++ flasktest/gw_quppa.py
@@ -1,50 +1,66 @@
 """
-VTEC Quppa driver
-Get interface to Quppa
+VTEC Quuppa driver
+Get interface to Quuppa
 author: Xin
-date:
+date: 11/4/2017
 License:
 """
 
 from urllib import request
-import time
 import json
 
 
-class VTECQuppa(object):
+class VTECQuuppa(object):
     """
     your comment here
     """
-    def __init__(self, ip='192.168.0.142'):
+    def __init__(self, ip='192.168.0.103'):
         self.ip = ip
         self.tag_id = None
 
-    def set_tag_id(self, id):
+    def set_tag_id(self, tag_id):
         """
         Set your tag id here
         id: int tag number
         """
-        self.tag_id = id
+        self.tag_id = tag_id
 
-    def get_location(self):
+    def read_quuppa_tagposition(self):
         with request.urlopen(
                                 'http://' +
                                 self.ip + ':8080/qpe/getTagPosition?version=2&humanReadable=true&maxAge=2000') as f:
-            data = f.read()
-            data_str = data.decode()
-            # print('Data:', data_str[0])
+            tagposition_all = f.read()
+        return tagposition_all
 
-            data_dic = json.loads(data_str)
-            print(data_dic['code'])
-            # tags_info = data_dic["tags"][0]
-            # print(tags_info['areaID])
-            # print('position: ', location)
+    def read_quuppa_taginfo(self):
+        with request.urlopen(
+                                'http://' +
+                                self.ip + ':8080/qpe/getTagInfo?version=2&humanReadable=true&maxAge=2000') as f:
+            taginfo_all = f.read()
+        return taginfo_all
 
-    def read_quppa(self):
-        pass
+    def get_tag_message(self):
+        data = self.read_quuppa_tagposition()
+        data_str = data.decode()
+        data_dic = json.loads(data_str)
+        return data_dic['message']
+
+    def get_location(self):
+        """
+        get the location of the tag, if you don't set the tag_id this function will return the locations of every tag
+        return: [x, y, z]
+        """
+        data = self.read_quuppa_tagposition()
+        data_str = data.decode()
+        data_dic = json.loads(data_str)
+        return data_dic['tags'][0]['position']
 
     def get_battery_voltage(self):
-        pass
+        """
+        get the battery voltage
+        return: float battery voltage
+        """
+        data = self.read_quuppa_tagposition()
 
     def get_status(self):
         if self.get_battery_voltage():
@@ -57,13 +73,9 @@ class VTECQuppa(object):
 
     def get_diameter(self):
         self.get_location()
-        #TODO calculate a diemsion
+        # TODO calculate a diemsion
 
 if __name__ == "__main__":
-    new_quppa = VTECQuppa()
-    new_quppa.set_tag_id(2)
-    new_quppa.get_location()
-    time.sleep(3)
-    new_quppa.get_location()
-    new_quppa.get_diameter()
-    new_quppa.get_battery_voltage()
+    test_quppa = VTECQuuppa()
+    tag_message = test_quppa.read_quuppa_taginfo()
+    print(tag_message)
